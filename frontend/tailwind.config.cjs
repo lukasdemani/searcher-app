35733d4aaed57f1da604564cb22e5ff591921de5
lukasdemani <demani.lukas@gmail.com>
2025-07-15 20:03:10 +0200

    migrate to vite

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -25,7 +23,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-} 
+  plugins: [require('@tailwindcss/forms')],
+};
