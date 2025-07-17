591b73b0f0fab77831471df4bb98949b5dcec937
Lukas Demani <demani.lukas@gmail.com>
2025-07-17 19:37:14 +0200

    Update 001_initial_schema.sql

--- backend/migrations/001_initial_schema.sql
+++ backend/migrations/001_initial_schema.sql
@@ -1,6 +1,4 @@
--- Initial schema for website analyzer
 
--- URLs table
 CREATE TABLE IF NOT EXISTS urls (
     id INT PRIMARY KEY AUTO_INCREMENT,
     url VARCHAR(2048) NOT NULL,
@@ -27,7 +25,6 @@ CREATE TABLE IF NOT EXISTS urls (
     INDEX idx_url_hash (url_hash)
 );
 
--- Broken links table
 CREATE TABLE IF NOT EXISTS broken_links (
     id INT PRIMARY KEY AUTO_INCREMENT,
     url_id INT NOT NULL,
